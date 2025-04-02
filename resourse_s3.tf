5686bd48dd78dc58552090c6fe48058c0622997b
rutwik <rutwiksatpute3034@gmail.com>
2025-04-02 23:35:39 +0530

    aws s3 storage

---
+++ resourse_s3.tf
@@ -0,0 +1,13 @@
+# i have created here s3 bucket to store data and teraform tf file.check 
+#also tf.state file auto maticlly updated in mentined bucket after evry action.
+
+resource "aws_s3_bucket" "Rg-rutwik" {
+  bucket = "rg-rutwik-1111"
+
+  tags = {
+    name        = "my_bucket"
+    envirnoment = "test"
+    region      = "ap-south-1"
+  }
+
+}
