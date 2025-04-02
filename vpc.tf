5c9108d2efa71962e8147de1d452b295dd55966f
rutwik <rutwiksatpute3034@gmail.com>
2025-04-03 00:37:50 +0530

    terraform VPC code written by rutwik

---
+++ vpc.tf
@@ -0,0 +1,29 @@
+resource "aws_vpc" "Rs-main" {
+    cidr_block = "10.100.0.0/24"
+
+    tags = {
+      name= "rs-main"
+      created_by="Rutwik"
+    }
+  
+}
+
+
+resource "aws_subnet" "rs_public" {
+    vpc_id = aws_vpc.Rs-main.id
+    cidr_block = "10.100.0.0/26"
+
+    tags = {
+      name= "public subnet"
+    }
+}
+
+resource "aws_subnet" "rs_private" {
+    vpc_id = aws_vpc.Rs-main.id
+    cidr_block = "10.100.1.0/26"
+
+    tags = {
+      name="private subnet"
+    }
+  
+}
